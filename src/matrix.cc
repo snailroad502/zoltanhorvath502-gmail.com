b80e71e4477584203ff9e4ec840437ccb1c292f0
Zoltan HORVATH <zhorvat@DESKTOP-2F112B1.localdomain>
2020-04-07 17:23:38 +0200

    implement basic functions +,*,transponate, add error hadling

--- src/matrix.cc
+++ src/matrix.cc
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <bits/stdc++.h>
-
+ 
 #include "matrix.h"
-
-using namespace std;
-
 
 template class Matrix<double>;
 template class LineVector<double>;
 
+using namespace std;
 
 template<class T>
 int Matrix<T>::read(ifstream& ifile)
@@ -17,9 +15,15 @@ int Matrix<T>::read(ifstream& ifile)
   int columnsn = 0;
   int rowsn = 1;
   LineVector<T> oneRow;
-  while(oneRow.read(ifile)) {
+
+  columnsn = oneRow.read(ifile);
+  while(columnsn) {
     rows.push_back(oneRow);
     rowsn++;
+    columnsn = oneRow.read(ifile);
+    if (columnsn != ncols() && columnsn > 0) {
+      throw SizeError();
+    }
   }
   return rowsn;
 }
@@ -27,13 +31,71 @@ int Matrix<T>::read(ifstream& ifile)
 template<class T>
 int Matrix<T>::write(ofstream& ofile)
 {
+    int rowsn = 1;
     for (auto c : rows)
     {  
         c.write(ofile); 
         ofile << endl;
+        rowsn++;
     }
-    return 0;
+    return rowsn;
 }
+
+template<class T>
+Matrix<T> Matrix<T>::transponate()
+{
+  Matrix<T> retval;
+  retval.rows.resize(ncols());
+  for (auto c : rows)
+  {
+    int i = 0;  
+    for (auto d : c.values)
+    {
+      retval.rows[i].values.push_back(d);
+      i++;
+    }
+ }
+  return retval;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator+(Matrix rhs)
+{
+    Matrix<T> retval;
+    if (ncols()!= rhs.ncols() || nrows() !=rhs.nrows())
+      throw SizeError();
+    for (auto it1 = rows.begin(), it2 = rhs.rows.begin(); it1 != rows.end(); it1++,
+    it2++)
+    {  
+      retval.rows.push_back(*it1 + *it2);
+    }
+    return retval;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator*(Matrix rhs)
+{
+    if (nrows()!= rhs.ncols())
+      throw SizeError();
+    Matrix<T> retval;
+    retval.rows.resize(nrows());
+    Matrix<T> tmp = rhs.transponate();
+    int i = 0;
+    for (LineVector<T> c : rows)
+    {
+      int j = 0;  
+      for (LineVector<T> d : rhs.rows)
+      {
+        T val = c * d;
+        retval.rows[i].values.push_back(val);
+        j++;
+      }
+      i++;
+    }
+    return retval;
+}
+
+
 
 template<class T>
 int LineVector<T>::read(ifstream& ifile)
@@ -62,8 +124,36 @@ int LineVector<T>::write(ofstream& ofile)
     for (auto c : values)
     {  
         ofile << c <<" ";
+        count++;
     }
-    return 0;
+    return count;
 }
 
+template<class T>
+T LineVector<T>::operator*(LineVector rhs)
+{
+    T retval;
+    if (values.size() != rhs.values.size())
+      throw SizeError();
+    for (auto it1 = values.begin(), it2 = rhs.values.begin(); it1 != values.end(); it1++,
+    it2++)
+    {  
+      retval = retval+ (*it1 * *it2);
+    }
+    return retval;
+}
 
+template<class T>
+LineVector<T> LineVector<T>::operator+(LineVector rhs)
+{
+    LineVector<T> retval;
+    if (values.size() != rhs.values.size())
+      throw SizeError();
+    for (auto it1 = values.begin(), it2 = rhs.values.begin(); it1 != values.end(); it1++,
+    it2++)
+    {  
+      retval.values.push_back(*it1 + *it2);
+    }
+    return retval;
+}
+  
--- src/matrix.h
+++ src/matrix.h
@@ -1,8 +1,9 @@
 using namespace std;
-
+template <class T> class Matrix;
 template <class T> class LineVector  {
+friend class Matrix<T>;
     std::vector<T> values;
-    T operator *(LineVector);
+    T operator *(LineVector<T>);
     LineVector operator *(T);
     LineVector operator +(LineVector);
 public:
@@ -13,11 +14,18 @@ public:
 
 template <class T> class Matrix  {
     std::vector< LineVector< T > > rows;
-    LineVector<T> operator *(LineVector<T>);
+public:
+    const int nrows() {return rows.size();};
+    const int ncols() {return rows.size() ? rows.begin()->values.size() : 0;};
+    Matrix transponate();
     Matrix operator *(Matrix);
     Matrix operator +(Matrix);
-public:
     int read(ifstream&);
     int write(ofstream&);
 };
+
+class MatrixError {};
+class ZeroDivide : public MatrixError {};
+class SizeError : public MatrixError {};
+
 
